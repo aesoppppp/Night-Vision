efa3995fc3aa4f265c3077c43d8b1db7fe8ccfc1
Hancho1577 <vojougae35@gmail.com>
2020-04-13 08:39:45 +0700

    Update NightVision.java

--- src/main/java/nightVision/NightVision.java
+++ src/main/java/nightVision/NightVision.java
@@ -11,18 +11,19 @@ public class NightVision extends PluginBase implements Listener{
 	private Effect effect;
 	@Override
 	public void onEnable() {
-		effect = Effect.getEffect(Effect.NIGHT_VISION).setVisible(false).setDuration(20 * 60 * 10);
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 			if (!(sender instanceof Player)) {
-				sender.sendMessage("§l§f[ §c! §f] 인게임에서만 가능합니다.");
+				sender.sendMessage("§l§f[ §c! §f] 인게임에서만 사용할 수 있습니다.");
 				return true;
 			}
+			effect = Effect.getEffect(Effect.NIGHT_VISION).setVisible(false).setDuration(20 * 60 * 10);
 			Player player = (Player) sender;
 			player.addEffect(effect);
+			player.sendMessage("§l§f[ §6! §f] 야간투시가 부여되었습니다.");
 		return true;
 	}
 }
